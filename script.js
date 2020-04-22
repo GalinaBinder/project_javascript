bd586208bfaa494c617fc17e3ca53ac570ae58fd
Galina Binder <marketing.russland@gmail.com>
2020-04-22 10:57:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var number = 5;
+var string = "Hallo!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+console.log(4 / 0);
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+
+}
+console.log(persone["name"]);
